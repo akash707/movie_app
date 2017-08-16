570df18c81251b5da3c011901e6110b93c3cf3c8
akash shukla <Akashshukla707@gmail.com>
2017-08-16 15:43:04 +0530

    final

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ var path=require('path');
 
 app.use(express.static(path.join('./'+__dirname+'/public')));
 
-   app.set('views', __dirname + '/assets/views');
-app.set('view engine','ejs');
 
 app.use(routes);
 
