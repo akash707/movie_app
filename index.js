9a1ca4943b70c15b9655b229739498c62536a4dc
akash707 <akashshukla707@gmail.com>
2017-08-17 02:27:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var path=require('path');
 
 app.use(express.static(path.join('./'+__dirname+'/public')));
 
+app.set('views', _dirname + '/assets/views');
+app.set('view engine','ejs');
 
 app.use(routes);
 
